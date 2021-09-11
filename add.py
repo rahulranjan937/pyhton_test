a737bcece4dfd6c86b65a11f817ac4ba82ecb913
Rahul Ranjan <40465492+rahulranjan937@users.noreply.github.com>
2021-09-11 20:35:23 +0530

    add_files

---
+++ add.py
@@ -0,0 +1,13 @@
+# Ohm's Law
+
+# Calculating Voltage
+def voltage(i,r):
+    return i * r
+
+# Calculating Resistance
+def resistance (v,i):
+    return v/i
+
+# Calculating Current
+def current(v,r):
+    return v/r
